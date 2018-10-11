5321d65abc186eaf78296033a679b8fa599947ab
shiliangl <shiliangwei@cloudolp.com>
2018-10-11 15:10:10 +0800

    2018年10月11日15:10:08

--- src/components/AudioPlayer/index.js
+++ src/components/AudioPlayer/index.js
@@ -4,9 +4,11 @@ import './index.scss'
 
 export default class AudioPlayer extends Component {
 
+  
   constructor(props) {
     super(props)
     this.state = {
+      curProgressBarWidth: 0,
       isPlaying: false
     }
   }
@@ -14,24 +16,38 @@ export default class AudioPlayer extends Component {
   clickToPlay = ()=>{
     this.setState({ isPlaying: !this.state.isPlaying })
     this.state.isPlaying ? this.audio.play() : this.audio.pause()
+    this.props.isPlaying(this.state.isPlaying )
+  }
+ 
+  syncTime(){
+    const currentTime = this.audio.currentTime
+    const duration = this.audio.duration
+    const timeScale = Math.round(currentTime) / Math.round(duration) * 100;
+    this.setState({
+      curProgressBarWidth: `${timeScale}%`,
+    })
   }
 
-  clickToPre(){
-    console.log('xx')
-  }
-
-  clickToNext(){
-    console.log('next')
+  setCurTime = (e) => {
+    const { left } = this.progressBar.getBoundingClientRect()
+    const distance = e.clientX - left
+    const scale = distance / this.progressBar.offsetWidth
+    // audio标签内有duration，数据对象中也有dt，不过dt = 1000 * duration
+    this.audio.currentTime = this.audio.duration * scale
+    this.setState({
+      curProgressBarWidth: `${scale * 100}%`,
+    })
   }
 
   componentDidMount() {
-    console.log(this.audio,'xx')
+    console.log(this.audio)
   }
 
   
   render() {
 
-    const { isPlaying, url, picUrl } = this.state
+    const { isPlaying, curProgressBarWidth } = this.state
+    const { url, picUrl, next, pre } = this.props
 
     return (
       <div className="AudioPlayer">
@@ -39,15 +55,41 @@ export default class AudioPlayer extends Component {
           <img src={ picUrl } alt="" /> 
         </div>
         <div className="player-btns"> 
-          <Icon onClick={this.clickToPre }  type="fast-backward" theme="outlined" />
+          <Icon onClick={() => pre()  }  type="fast-backward" theme="outlined" />
           <Icon onClick={this.clickToPlay} type={ isPlaying ? 'play-circle' :'pause-circle' }  theme="outlined" />
-          <Icon onClick={this.clickToNext }  type="fast-forward" theme="outlined" />
+          <Icon onClick={() => next() }  type="fast-forward" theme="outlined" />
         </div>
-        <div className="player-state"></div>
+        <div className="player-state">
+          <div className="player-state-top"> </div>
+          <div className="player-state-bottom"> 
+            <div className="progress-bar" ref={node => this.progressBar = node} onClick={(e) => {this.setCurTime(e) }}>
+              <div style={{ width: `${curProgressBarWidth}` }} className="current-progress"></div>
+            </div>
+          </div>
+        </div>
         <div className="player-voice"> </div>
         <div className="player-extra"> </div>
 
-        <audio style={{ display: 'none' }} ref={(audio) => { this.audio = audio } } src={ url }></audio>
+        <audio 
+          ref={
+            audio => this.audio = audio  
+          }
+          onTimeUpdate={
+            ()=> this.syncTime()
+          }
+          onEnded ={
+            ()=> {
+              next()
+              console.log('播放完毕')
+            }
+          }
+          onError={
+            e=> {
+              console.log(e)
+            }
+          }
+          src={ url }>
+        </audio>
       </div>
     )
   }
